ab115146e6971b033ce0e539b4ce2e66f0252872
Bhussai05 <b2hussain@torontomu.ca>
2026-05-14 13:58:39 -0400

    temp changes

--- langevin.py
+++ langevin.py
@@ -3,17 +3,26 @@ import signalz
 import matplotlib.pyplot as plt
 
 epsilon = 1
-mu = 2
-t = np.linspace(0, 10000, 10)
-x_0 = np.zeros(len(t))
-
-
-eta = signalz.brownian_noise(10, leak=0.1, start=0, std=1, source="gaussian")
+muu = 2
+t0 = np.linspace(0, 10000, 1000)
+x0= 6
 
 
 
+etaa = signalz.brownian_noise(1000, leak=0.1, start=0, std=1, source="gaussian")
 
-    
+
+def langevin(x_0, e, mu, t, eta):
+    xdot = np.zeros(len(t))
+    xdot =  e*(1-2*x_0) + np.sqrt(2*mu*x_0*(1-x_0))*eta
+
+    for i in range(len(t)):
+        xdot[i] = e*(1-2*xdot[i-1]) + np.sqrt(2*mu*xdot[i-1]*(1-xdot[i-1]))*eta
+
+langevin(x0, epsilon, muu, t0, etaa)
+
+
+
 
 
     
